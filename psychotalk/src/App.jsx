77d91f7ebf19dd497863272e1daf232a2a67c703
SANTHI <santhiroja1408@gmail.com>
2024-10-10 23:23:10 +0530

    first commit for footer component

--- psychotalk/src/App.jsx
+++ psychotalk/src/App.jsx
@@ -2,14 +2,17 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Footer from './footer'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css'
 
 function App() {
- 
   return (
     <>
       <div>
        <h1>Psychotalk</h1>
       </div>
+      <Footer/>
     </>
   )
 }
